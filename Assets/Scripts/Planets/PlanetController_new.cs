7ecf0385531cb7d9146a7f1dffd436849ff9aa3f
si3792 <simo.t.iliev@gmail.com>
2017-01-21 17:58:03 +0200

    New planet rotation script

---
+++ Assets/Scripts/Planets/PlanetController_new.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Scripts/Planets/Planet controller new")]
+public class PlanetController_new : MonoBehaviour {
+
+	public float rotationSpeed = 5f;
+	public bool rotateClockwise = true;
+	private GameObject sun; // The object we rotate around
+	private Rigidbody2D rb;
+
+	void Start () {
+		rb = this.gameObject.GetComponent<Rigidbody2D> ();
+		sun = this.gameObject.GetComponent<Transform> ().parent.gameObject;
+	}
+	
+	void rotateAroundSun() {
+
+		transform.position = Vector2.Lerp(transform.position,
+			transform.position + Quaternion.Euler(0,0, ((rotateClockwise)? -90f:90f)) * (sun.transform.position - transform.position).normalized,
+			Time.deltaTime * rotationSpeed);
+		
+	}
+
+	void FixedUpdate () {
+
+		rotateAroundSun ();
+	}
+		
+
+}
