e7ab54cf079dbb6165d096717d88ba8e39812bbf
KatNotDinner <gogo.100@abv.bg>
2017-01-21 23:10:13 +0200

    Stars (for the 100th time)

---
+++ Assets/Scripts/Player/Upgrades/DamageUpgrade.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageUpgrade : MonoBehaviour, IUpgrade {
+    public void execute() {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
