55481fd0954ee51207037c9c885da033ffc21cc4
si3792 <simo.t.iliev@gmail.com>
2017-01-22 00:49:11 +0200

    WOrk on parallax

---
+++ Assets/RandomRotationAndSize.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotationAndSize : MonoBehaviour {
+
+	public bool randomizeRotation;
+	public bool randomizeSize;
+	public float minSize= 1.0f;
+	public float maxSize = 1.0f;
+
+	void Start () {
+		if(randomizeRotation)transform.Rotate ( new Vector3(0, 0, Random.Range (0, 360)));
+		if (randomizeSize) {
+			float size = Random.Range (minSize, maxSize);
+			transform.localScale = new Vector3 (size, size, 1); 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/RandomSprite.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour {
+
+	public Sprite[] mSprites;
+	public int mustEqSize;
+
+	public void randomize()
+	{
+		int a = Random.Range (0, mustEqSize);
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = mSprites [a];
+	}
+
+	void Start() {
+		randomize ();
+	}
+
+}
