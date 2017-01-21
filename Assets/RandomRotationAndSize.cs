82a8d52778d7600195841146f9df01a665d4c442
si3792 <simo.t.iliev@gmail.com>
2017-01-22 01:35:23 +0200

    Improved parallax

--- Assets/RandomRotationAndSize.cs
+++ Assets/RandomRotationAndSize.cs
@@ -6,14 +6,20 @@ public class RandomRotationAndSize : MonoBehaviour {
 
 	public bool randomizeRotation;
 	public bool randomizeSize;
+	public bool randomizeDepth = false;
 	public float minSize= 1.0f;
 	public float maxSize = 1.0f;
+	public float minDepth;
+	public float maxDepth;
 
 	void Start () {
 		if(randomizeRotation)transform.Rotate ( new Vector3(0, 0, Random.Range (0, 360)));
 		if (randomizeSize) {
 			float size = Random.Range (minSize, maxSize);
 			transform.localScale = new Vector3 (size, size, 1); 
+		}
+		if(randomizeDepth) {
+			transform.position = new Vector3 (transform.position.x, transform.position.y, Random.Range(minDepth, maxDepth));
 		}
 	}
 	
