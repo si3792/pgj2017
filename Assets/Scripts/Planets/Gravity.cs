ccf51efa3cac2de795d27717b040f4449767f6bd
KatNotDinner <gogo.100@abv.bg>
2017-01-21 02:58:06 +0200

    Removed .meta files

---
+++ Assets/Scripts/Planets/Gravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+    private PlayerMovement plrMovement;
+    private Transform plrTransform;
+    private float size = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        plrMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        plrTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        Debug.Log("FOund player" + plrMovement);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        plrMovement.ApplyGravity(plrTransform.transform.position, 1.0f);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float rotationMultiplier = 0.1f;
+    public float speedMultiplier = 1.0f;
+
+    public void ApplyGravity(Vector3 pos, float size) {
+        float distance = Vector2.Distance(pos, transform.position);
+
+        rb.AddForce((pos - transform.position) * distance)  ;
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        rb.AddTorque(Input.GetAxis("Horizontal") * rotationMultiplier * -1);
+
+        // Controls 
+        rb.AddForce(Quaternion.Euler(0, 0, transform.rotation.eulerAngles.z) * new Vector2(0, Input.GetAxis("Vertical") * speedMultiplier), ForceMode2D.Force);
+
+        // TODO: Add gravity
+	}
+}
