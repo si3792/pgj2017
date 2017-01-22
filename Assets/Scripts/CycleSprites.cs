b1cae6f1355040068e68bb177789c58dd9052d17
si3792 <simo.t.iliev@gmail.com>
2017-01-22 02:26:33 +0200

    added smoke effect

---
+++ Assets/Scripts/CycleSprites.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleSprites : MonoBehaviour {
+
+
+	public int curSprite = 0;
+	public Sprite[] mSprites;
+	public int mustEqSize;
+	public int waitFrames = 1;
+	public int curWait = 0;
+
+
+	void nextSprite() {
+		curSprite++;
+		if (curSprite >= mustEqSize)
+			curSprite = 0;
+
+		this.gameObject.GetComponent<SpriteRenderer> ().sprite = mSprites [curSprite];
+	}
+
+	void FixedUpdate() {
+
+		curWait++;
+		if (curWait > waitFrames) {
+			nextSprite ();
+			curWait = 0;
+		}
+	}
+}
