b2056fa4e24cee76dd6c00c559d15ed9de4835b4
si3792 <simo.t.iliev@gmail.com>
2017-01-22 11:03:15 +0200

    added sun glow

--- Assets/ConfigureGlow.cs
+++ Assets/ConfigureGlow.cs
@@ -2,20 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomSprite : MonoBehaviour {
+public class ConfigureGlow : MonoBehaviour {
 
 	public Sprite[] mSprites;
 	public int mustEqSize;
-	public int chosenSprite = 0;
+	public int chosenSprite;
 
-	public void randomize()
-	{
-		chosenSprite = Random.Range (0, mustEqSize);
+	void FixedUpdate() {
+
+		chosenSprite = transform.parent.GetComponent<RandomSprite>().chosenSprite;
 		this.gameObject.GetComponent<SpriteRenderer> ().sprite = mSprites [chosenSprite];
-	}
 
-	void Start() {
-		randomize ();
 	}
-
 }
