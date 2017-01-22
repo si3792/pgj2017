3040c4b6f2688afeb566256c3c6c83e1444909d4
KatNotDinner <gogo.100@abv.bg>
2017-01-22 06:07:07 +0200

    Rings now have custom speed

--- Assets/CreateSolarSystem.cs
+++ Assets/CreateSolarSystem.cs
@@ -18,7 +18,8 @@ public class CreateSolarSystem : MonoBehaviour {
         UnityEngine.Random.InitState(DateTime.Now.Millisecond);
 
         for (int i = 3; i < planetsNumber; i++) {
-            generateRing(5, defaultDistance * i, (UnityEngine.Random.value > 0.5f));
+            float rotSpeed = UnityEngine.Random.Range(0.1f, 0.3f);
+            generateRing(5, defaultDistance * i, (UnityEngine.Random.value > 0.5f), rotSpeed);
        }
 
     }
@@ -28,17 +29,17 @@ public class CreateSolarSystem : MonoBehaviour {
 		
 	}
 
-    void generateRing(float numOfPlanets, float dist, bool clockwise) {
+    void generateRing(float numOfPlanets, float dist, bool clockwise, float rotSpeed) {
 
         float startAngle = UnityEngine.Random.Range(0f, 360f);
         float tmpAng = 360 / numOfPlanets;
         for (int i = 1; i <= numOfPlanets; i++) {
-            generatePlanet(dist, i * tmpAng + startAngle, clockwise);
+            generatePlanet(dist, i * tmpAng + startAngle, clockwise, rotSpeed);
         }
 
     }
 
-    void generatePlanet(float dist, float angle, bool clockwise) {
+    void generatePlanet(float dist, float angle, bool clockwise, float rotSpeed) {
 
     
         GameObject temp = Instantiate(planetPrefab, transform.position, Quaternion.identity);
@@ -49,7 +50,8 @@ public class CreateSolarSystem : MonoBehaviour {
             temp.transform.position.y + dist * (float)(Math.Sin(angle)), 0);
         temp.transform.SetParent(transform);
         temp.GetComponent<PlanetController_new>().rotateClockwise = clockwise;
-        
+        temp.GetComponent<PlanetController_new>().rotationSpeed = rotSpeed;
+
         /* float distanceToCenter = Vector3.Distance(temp.transform.position, center);
          temp.transform.SetParent(transform);
          temp.transform.position = new Vector3(distanceToCenter + dist,
