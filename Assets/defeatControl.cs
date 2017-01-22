9a768a5e114f6b977e0cd6c1a3c2ae563c723a04
si3792 <simo.t.iliev@gmail.com>
2017-01-22 13:20:32 +0200

    Added game loop

---
+++ Assets/defeatControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defeatControl : MonoBehaviour {
+
+	public Transform camera;
+	// Use this for initialization
+	void Start () {
+		transform.parent = camera;
+	}
+
+	void switchRoom() {
+		Application.LoadLevel ("menu");
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.position = new Vector3 (camera.position.x, camera.position.y, camera.position.z + 10); 
+		camera.transform.Translate (0f, 0f, -0.1f);
+		Invoke ("switchRoom", 2.5f);
+	}
+}
