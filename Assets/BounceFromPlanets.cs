c1a968e6b6efcb454ea9e8ac436c9bd9f652b315
KatNotDinner <gogo.100@abv.bg>
2017-01-22 07:13:11 +0200

    added collision

---
+++ Assets/BounceFromPlanets.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceFromPlanets : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Rotatable") {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
