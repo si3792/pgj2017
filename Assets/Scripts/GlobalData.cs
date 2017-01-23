fbf61e67394e16c84f7e16670f539ed46d2b843d
si3792 <simo.t.iliev@gmail.com>
2017-01-24 00:17:47 +0200

    Final presentation version

--- Assets/Scripts/GlobalData.cs
+++ Assets/Scripts/GlobalData.cs
@@ -6,12 +6,12 @@ public class GlobalData : MonoBehaviour {
 	public static bool debug = true;
     public static float gravityFactor = 1.0f;
     public static float gravityCutoffDistance = 100f;
-    public static float rotationFactor = 0.02f;
+    public static float rotationFactor = 0.01f;
     public static float healthUpgradeFactor = 0.75f;
     public static float speedUpgradeFactor = 0.75f;
     public static float damageUpgradeFactor = 0.75f;
     public static float bounceFactor = 700f;
-    public static int dmgOnHit = 100;
+    public static int dmgOnHit = 50;
     public static float chargeDrainFactor = 0.1f;
 	public static float radioChancePercentage = 16;
 
