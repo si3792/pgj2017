bc43e95391765690901e22970ef1bd72c6e6a719
si3792 <simo.t.iliev@gmail.com>
2017-01-25 03:09:02 +0200

    improvements

---
+++ Assets/UIHackCell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIHackCell : MonoBehaviour {
+
+	public int required = 1;
+
+	void Start () {
+				
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate() {
+		if (required <= GlobalData.transmissionsSpied)
+			gameObject.GetComponent<Image> ().enabled = true;
+	}
+}
